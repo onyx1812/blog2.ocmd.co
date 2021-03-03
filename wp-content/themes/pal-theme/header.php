d3927be9ab169df69ae10b80ab2a038418cfc160
MaxGloba OCMD <max@purpleadlab.com>
2021-03-03 05:47:26 +0000

    add changes to header from server

--- wp-content/themes/pal-theme/header.php
+++ wp-content/themes/pal-theme/header.php
@@ -38,11 +38,26 @@
       fbq('track', 'PageView');
     </script>
     <!-- End Facebook Pixel Code -->
+      
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NKQX69B');</script>
+<!-- End Google Tag Manager -->
+      
+      
 
     <?php wp_head(); ?>
   </head>
 
   <body <?php body_class(); ?> >
+      
+      <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKQX69B"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <header class="main-header">
       <div class="container">
