22f90a071569fd8d502c3db615dff7fd9bbab7c6
massagehero <onyx18121990@gmail.com>
2021-03-10 14:32:39 +0200

    added new template

--- wp-content/themes/pal-theme/js/scripts-blog_fb.js
+++ wp-content/themes/pal-theme/js/scripts-blog_fb.js
@@ -20,9 +20,3 @@ closePopup.addEventListener('click', function(e){
 document.querySelector('.nav-btn').onclick = e => {
   document.querySelector('.nav-top').classList.toggle('nav-top-hide');
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(()=>{
-    document.querySelector('.btn-video').classList.add('btn-video-show');
-  }, 10000);
-});
